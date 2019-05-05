c7aa169e921ce88c8e79565e0a1974e0a1161736
Mark <18009838166@163.com>
2019-05-05 09:28:08 +0800

    add new source.

---
+++ src/main/java/com/thread/MyRunnable.java
@@ -0,0 +1,21 @@
+package com.thread;
+
+import java.util.concurrent.TimeUnit;
+
+public class MyRunnable implements Runnable {
+
+    @Override
+    public void run() {
+        Thread currentThread = Thread.currentThread();
+        System.out.println(currentThread.getName() + "-------------进入");
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println(currentThread.getName() + "-------------离开");
+        }
+
+    }
+}
