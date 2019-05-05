7f0c7ef8d5f9b541e3b16d4416cd107d03d95108
Mark <18009838166@163.com>
2019-05-05 11:42:41 +0800

    add a java file

--- src/main/java/com/java8/NewFeatures.java
+++ src/main/java/com/java8/NewFeatures.java
@@ -3,6 +3,9 @@
  */
 package com.java8;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author 18009
  * @see https://www.oracle.com/technetwork/java/javase/8-whats-new-2157071.html
@@ -12,5 +15,10 @@ package com.java8;
 public class NewFeatures {
   public static void main(String[] args) {
     // Java SE 8 Features and Enhancements
+    List<Integer> primes = Arrays.asList(new Integer[]{2, 3,5,7});
+    int factor = 2;
+    primes.forEach(element -> { System.out.println(factor*element); });
+    primes.forEach(element -> System.out.println(factor*element));
+    primes.forEach(e -> System.out.println("Hello World " + e));
   }
 }
