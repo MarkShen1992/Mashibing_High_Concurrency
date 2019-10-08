b2f196cf4c8a27812239152e93bada94f2c5f5da
e <18009838166@163.com>
2019-10-08 11:45:29 +0800

    introduce the feature of JDK13

---
+++ src/main/java/com/java13/NewFeatures.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.java13;
+
+/**
+ * @author MarkShen
+ * @see https://www.oracle.com/technetwork/java/13-relnote-issues-5460548.html
+ */
+public class NewFeatures {
+  public static void main(String[] args) {
+    // Java SE 13 Features and Enhancements
+  }
+}
