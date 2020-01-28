01c1140216ab032816493985ab8d014281e84dc3
e <18009838166@163.com>
2020-01-28 18:42:28 +0800

    The art of Java Concurrency Programming: chapter01

---
+++ src/main/java/com/art/concurency/ch01/ConcurrencyTest.java
@@ -0,0 +1,56 @@
+package com.art.concurency.ch01;
+
+/**
+ * 1. 循环次数
+ * 2. 串并行程序实行时间
+ * 3. Lmbench3 http://www.bitmover.com/lmbench/ 上下文切换的时长
+ * 4. vmstat 上下文切换的次数
+ * 5. 如何减少上下文切换
+ *  无锁并发编程， CAS算法， 使用最少线程， 使用协程
+ *  无锁并发编程：不同线程处理不同段的数据
+ */
+public class ConcurrencyTest {
+    private static final long count = 100001;
+
+    public static void main(String[] args) throws InterruptedException {
+        concurrency();
+        serial();
+    }
+
+    private static void serial() {
+        long start = System.currentTimeMillis();
+        int a = 0;
+        for (long i = 0; i < count; i++) {
+            a += 5;
+        }
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        System.out.println("serial: " + time);
+    }
+
+    private static void concurrency() throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int a = 0;
+                for (long i = 0; i < count; i++) {
+                    a += 5;
+                }
+            }
+        });
+        thread.start();
+
+        int b = 0;
+        for (long i = 0; i < count; i++) {
+            b--;
+        }
+        long time = System.currentTimeMillis() - start;
+        thread.join();
+        System.out.println("concurrency: " + time);
+    }
+
+}
---
+++ src/main/java/com/art/concurency/ch01/DeadLockDemo.java
@@ -0,0 +1,46 @@
+package com.art.concurency.ch01;
+
+public class DeadLockDemo {
+    private static String A = "A";
+    private static String B = "B";
+
+    public static void main(String[] args) {
+        new DeadLockDemo().deadLock();
+    }
+
+    private void deadLock() {
+        Thread t1 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (A) {
+                    try {
+                        Thread.sleep(2000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (B) {
+                        System.out.println("1");
+                    }
+                }
+            }
+        });
+
+        Thread t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                synchronized (B) {
+                    try {
+                        Thread.sleep(9000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    synchronized (A) {
+                        System.out.println("2");
+                    }
+                }
+            }
+        });
+        t1.start();
+        t2.start();
+    }
+}
