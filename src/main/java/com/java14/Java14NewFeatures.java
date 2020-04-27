2b518ce410973e00185d174bcf61628b75023375
e <18009838166@163.com>
2020-04-27 09:35:32 +0800

    Java 14 new features

---
+++ src/main/java/com/java14/Java14NewFeatures.java
@@ -0,0 +1,12 @@
+package com.java14;
+
+/**
+ * @author MarkShen1992
+ * @see https://www.oracle.com/technetwork/java/javase/14all-relnotes-5809668.html
+ * @see https://www.codejava.net/java-se/java-14-new-features
+ */
+public class Java14NewFeatures {
+    public static void main(String[] args) {
+
+    }
+}
