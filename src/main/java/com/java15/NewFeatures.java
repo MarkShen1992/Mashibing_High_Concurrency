8a053fc7e3b03cd52e28a0c85dd23ff0c7c9235e
e <18009838166@163.com>
2020-09-21 06:33:46 +0800

    jdk 15 new features

---
+++ src/main/java/com/java15/NewFeatures.java
@@ -0,0 +1,14 @@
+package com.java15;
+
+/**
+ * jdk15 new feature
+ *
+ * https://openjdk.java.net/projects/jdk/15/
+ * https://javaalmanac.io/jdk/15/
+ * https://www.youtube.com/watch?v=uTNjeM0G9Nk
+ */
+public class NewFeatures {
+    public static void main(String[] args) {
+        System.out.println("jdk 15 new features.");
+    }
+}
