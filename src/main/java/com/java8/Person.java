64086f8d5f56ef302a110058321376fc42ef73ab
e <18009838166@163.com>
2021-01-17 09:04:38 +0800

    Stream API learn 03

--- src/main/java/com/java8/Person.java
+++ src/main/java/com/java8/Person.java
@@ -3,12 +3,21 @@ package com.java8;
 public class Person {
     private int id;
     private String name;
+    private int age;
+    private int sex;
 
     public Person() {}
 
     public Person(int id, String name) {
         this.id = id;
         this.name = name;
+    }
+
+    public Person(int id, String name, int age, int sex) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.sex = sex;
     }
 
     public int getId() {
@@ -27,11 +36,29 @@ public class Person {
         this.name = name;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getSex() {
+        return sex;
+    }
+
+    public void setSex(int sex) {
+        this.sex = sex;
+    }
+
     @Override
     public String toString() {
         return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                '}';
+            "id=" + id +
+            ", name='" + name + '\'' +
+            ", age='" + age + '\'' +
+            ", sex=" + sex +
+            '}';
     }
 }
