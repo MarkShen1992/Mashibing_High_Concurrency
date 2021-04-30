06996d254fa26567d9abc8356353aedc447142a4
e <18009838166@163.com>
2021-05-01 06:42:52 +0800

    jdk 16 new features.

---
+++ src/main/java/com/java16/Java16NewFeatures.java
@@ -0,0 +1,13 @@
+package com.java16;
+
+/**
+ * @author MarkShen
+ */
+public class Java16NewFeatures {
+
+    public static void main(String[] args) {
+        // Java 16 new features
+        // 1) https://openjdk.java.net/projects/jdk/16/
+        // 2) https://mkyong.com/java/what-is-new-in-java-16/
+    }
+}
