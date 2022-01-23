84eac35d5c05a38862a570387b0355adca620e43
shenjy <markshen1992@gmail.com>
2022-01-23 17:59:56 +0800

    feature: Java 17 new features.

---
+++ src/main/java/com/java17/Java17NewFeatures.java
@@ -0,0 +1,13 @@
+package com.java17;
+
+/**
+ * https://www.baeldung.com/java-17-new-features https://mkyong.com/java/what-is-new-in-java-17/
+ * https://www.geeksforgeeks.org/jdk-17-new-features-in-java-17/
+ *
+ * @author MarkShen
+ */
+public class Java17NewFeatures {
+    public static void main(String[] args) {
+        System.out.println("Java 17 new Features.");
+    }
+}
